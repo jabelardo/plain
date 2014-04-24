6f202624cd06bafcba03ceb0ef0e9b2c6c7b0083
Michael Wellner <michael.wellner@de.ibm.com>
2014-04-24 23:35:31 +0200

    Started SpacesClient implementation ...

--- plain-integration/src/main/scala/com/ibm/plain/integration/spaces/SpacesClient.scala
+++ plain-integration/src/main/scala/com/ibm/plain/integration/spaces/SpacesClient.scala
@@ -6,13 +6,11 @@ package integration
 
 package spaces
 
-import org.apache.http.HttpResponse
-import org.apache.http.client.ResponseHandler
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
 
 import logging.Logger
+import java.io.FileOutputStream
+import java.nio.file.Path
+import java.util.UUID
 
 /**
  *
@@ -21,21 +19,49 @@ final class SpacesClient private
 
   extends Logger {
 
-  def doGet(uri: String) = {
-    val client = HttpClients.createDefault
-    try {
-      val request = new HttpGet(uri)
-      val handler = new ResponseHandler[String] {
-        def handleResponse(response: HttpResponse): String = {
-          response.getStatusLine.getStatusCode match {
-            case 200 ⇒ info(EntityUtils.toString(response.getEntity))
-            case e ⇒ error("status code " + e)
-          }
-          null
-        }
-      }
-      client.execute(request, handler)
-    } finally client.close
+  //  def doGet(uri: String) = {
+  //    val client = HttpClients.createDefault
+  //    try {
+  //      val request = new HttpGet("http://harryklein.munich.de.ibm.com:7070/spaces/blabla?blabla")
+  //      request.setHeader(HttpHeaders.ACCEPT_ENCODING, "deflate")
+  //
+  //
+  //      val handler = new ResponseHandler[String] {
+  //        def handleResponse(response: HttpResponse): String = {
+  //          response.getStatusLine.getStatusCode match {
+  //            case 200 ⇒
+  //              try {
+  //                info(response.getEntity.getContentLength.toString)
+  //                println("A")
+  //                val f = new File("/Users/michael/Downloads/bla3.tar.gz")
+  //                val out = new FileOutputStream(f)
+  //
+  //                info(f.getAbsolutePath)
+  //
+  //                println(response.getEntity)
+  //                val in = response.getEntity.getContent
+  //                println(in)
+  //                println(io.copyBytesIo(in, out))
+  //                out.close
+  //                println("FERTSCH!")
+  //              } catch {
+  //                case e: Throwable => println(e)
+  //              }
+  //            case e ⇒ error("status code " + e)
+  //          }
+  //          null
+  //        }
+  //      }
+  //      client.execute(request, handler)
+  //    } finally client.close
+  //  }
+
+}
+
+case class SpacesURI(node: String, container: String = UUID.randomUUID) {
+
+  def uri = {
+    s"spaces://$node/$container"
   }
 
 }
@@ -45,6 +71,30 @@ final class SpacesClient private
  */
 object SpacesClient {
 
-  final def get(uri: String) = new SpacesClient().doGet(uri)
+  final def get(uri: SpacesURI, file: Path, relativePath: Option[Path] = None) = ???
+
+  /**
+   * Uploads a resource to the given URI.
+   *
+   * @param uri
+   * The target URI.
+   * @param file
+   * The file or directory which should be uploaded.
+   * @param relativePath
+   * The relative path within the container.
+   * @return
+   */
+  final def put(uri: SpacesURI, file: Path, relativePath: Option[Path] = None) = ???
+
+  /**
+   * Deletes a resource in the given space.
+   *
+   * @param uri
+   * The spaces URI on which the operation should be excuted.
+   * @param relativePath
+   * The path relativ to the container.
+   * @return
+   */
+  final def delete(uri: SpacesURI, relativePath: Option[Path] = None): Boolean = ???
 
 }
