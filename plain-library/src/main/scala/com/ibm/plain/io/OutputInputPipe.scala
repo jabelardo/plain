2efa5bbf4c19bbb56fe9295e94ad686f9ee82cae
Michael Wellner <info@michaelwellner.de>
2014-05-09 13:53:46 +0200

    Created OutputInputPipe

---
+++ plain-library/src/main/scala/com/ibm/plain/io/OutputInputPipe.scala
@@ -0,0 +1,14 @@
+package com.ibm.plain.io
+
+/**
+ * Implements the Circular Buffer producer/consumer model for bytes.
+ *
+ * Inspired by http://ostermiller.org/utils/src/CircularByteBuffer.java.html.
+ *
+ * @author michael.wellner@de.ibm.com
+ */
+class OutputInputPipe {
+
+  // TODO
+
+}
